0ee198463e4befaa44589f522b3e8f373c1e060c
ghentilpixels <okrahjackson@gmail.com>
2026-07-11 08:54:03 +0000

    prisma generate error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,8 @@ import "dotenv/config";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
+export const prisma = globalForPrisma.prisma || new PrismaClient(
+  process.env.DATABASE_URL ? { datasourceUrl: process.env.DATABASE_URL } : {}
+);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
